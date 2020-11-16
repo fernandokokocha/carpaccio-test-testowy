2c9d5432e6ef2f897276ffeff8717981eb354876
Bartosz Krajka <krajka.bartosz@gmail.com>
2020-11-16 16:34:17 +0100

    Eh a tutaj test nie przechodzi

--- src/carpaccio.test.js
+++ src/carpaccio.test.js
@@ -2,10 +2,18 @@ function carpaccio(a, b) {
   return a + b;
 }
 
-test("adds 1 + 2 to equal 3", () => {
+test("a", () => {
   expect(carpaccio(1, 2)).toBe(3);
 });
 
-test("adds 2 + 3 to equal 5", () => {
+test("b", () => {
   expect(carpaccio(2, 3)).toBe(5);
+});
+
+test("c", () => {
+  expect(carpaccio(3, 4)).toBe(7);
+});
+
+test("d", () => {
+  expect(carpaccio(3, 4)).toBe(70);
 });
